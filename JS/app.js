cdb98be42cdb1a0e0e4a52479144f27229bfce86
mehrzad sajjadi <canadavancouver2003@gmail.com>
2025-01-02 16:55:05 +0330

    add Oil and Win-time

--- JS/app.js
+++ JS/app.js
@@ -3,11 +3,43 @@ const ctx = canvas.getContext("2d");
 
 let start = document.getElementById("start");
 let refresh = document.getElementById("refresh");
+
+
+let oilNum = document.getElementById("oil");
+
+let limitOil = parseInt(oilNum.textContent);
+
+let timeNum = document.getElementById("time");
+
+let limitTime = parseInt(timeNum.textContent);
+
 start.addEventListener("click", () => {
     animate();
     refresh.style.display = "inline";
     start.style.display = "none";
+
+    //کاهش سوخت
+    setInterval(() => {
+        limitOil = limitOil - 1;
+        if (limitOil == 0) {
+            location.reload();
+        }
+        oilNum.textContent = limitOil;
+    }, 1000);
+
+    //زمان پیروزی
+    setInterval(() => {
+        limitTime = limitTime - 1;
+        if (limitTime == 0) {
+            location.reload();
+            alert("تبریک میگم شما بردید !")
+        }
+        timeNum.textContent = limitTime;
+    }, 1000);
+
 });
+
+
 refresh.addEventListener("click", () => {
     window.location.reload();
 });
@@ -23,13 +55,16 @@ planeImg.src = "./img/plane.webp";
 // موقعیت و اندازه‌ی هواپیما
 let planeX = canvas.width / 2;
 let planeY = canvas.height - 60;
-const planeWidth = 60;
-const planeHeight = 60;
+let planeWidth = 60;
+let planeHeight = 60;
 
 let bullets = [];
 let stons = [];
+let oils = [];
+
 
 setInterval(RainStone, 2000);
+setInterval(RainOil, 5000);
 
 
 function animate() {
@@ -66,19 +101,21 @@ function animate() {
                     b.x < s.x + 25 && b.x + 2 > s.x
                 )
                 &&
-                (b.y < s.y + 25 && b.y + 25 > s.y)
+                (
+                    b.y < s.y + 25 && b.y + 25 > s.y
+                )
             ) {
                 // حذف تیر و شهاب‌سنگ
                 bullets.splice(i, 1);
                 stons.splice(j, 1);
-                break; // از حلقه شهاب‌سنگ خارج شود
+                break;
             }
         }
 
     }
 
 
-    //شهاب سنگ
+    //شهاب سنگ باریدن
     ctx.fillStyle = "orange";
     for (let o = stons.length - 1; o > 0; o--) {
         var s = stons[o];
@@ -88,16 +125,63 @@ function animate() {
     }
 
 
+    //بارش سوخت
+    ctx.fillStyle = "white";
+    for (let t = oils.length - 1; t >= 0; t--) {
+        var oil = oils[t];
+        ctx.fillRect(oil.x, oil.y, 25, 25);
+        //سرعت پالیین اومدن سوخت
+        oil.y = oil.y + 2;
+    }
+
+    // تشخیص برخورد هواپیما یا شهاب
+    for (let r = stons.length - 1; r >= 0; r--) {
+        let s = stons[r];
+        if (
+            (
+                planeX < s.x + 25 &&
+                planeX + planeWidth > s.x
+            )
+            &&
+
+            (planeY < s.y + 25 &&
+                planeY + planeHeight > s.y)
+        ) {
+            location.reload();
+            break;
+            alert("متاسفانه پیروز نشدید !")
+
+        }
+    }
+
+
+
+
+    // تشخیص برخورد هواپیما یا سوخت
+    for (let k = oils.length - 1; k >= 0; k--) {
+        let oil = oils[k];
+        if (
+            (
+                planeX < oil.x + 25 &&
+                planeX + planeWidth > oil.x
+            )
+            &&
+
+            (planeY < oil.y + 25 &&
+                planeY + planeHeight > oil.y)
+        ) {
+            oils.splice(k, 1);
+            limitOil = limitOil + 10 ;
+            oilNum.textContent = limitOil;
+
+        }
+    }
+
 }
 
 let bulletNum = document.getElementById("bullet");
 
-// if (b.y == s.y && s.x == s.x) {
-//     bullets.splice(b, 1)
-//     stons.splice(s, 1)
-// }
-
-
+//شلیک تیر
 function BulletThrow() {
     let limitBullet = parseInt(bulletNum.textContent);
 
@@ -111,7 +195,8 @@ function BulletThrow() {
         });
 
         limitBullet = limitBullet - 1;
-        bulletNum.textContent = limitBullet;
+        bulletNum.t
+        extContent = limitBullet;
 
     }
 }
@@ -127,8 +212,22 @@ function RainStone() {
 }
 
 
+function RainOil() {
+
+    let oilX = Math.floor(Math.random() * (canvas.width - 1));
+    let oilY = canvas.height - (canvas.height + 10);
+    oils.push({
+        x: oilX,
+        y: oilY,
+    });
+
+}
+
+
+
+
 document.addEventListener("keydown", (event) => {
-    const step = 15; // گام های جابجایی
+    const step = 30; // گام های جابجایی
     if (event.key == "ArrowLeft") {
         planeX = planeX - step;
     } else if (event.key == "ArrowRight") {
