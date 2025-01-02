39579c074834ffb391216cc97bbdc3819dd6380a
amirhosein sajjadi <canadavancouver2003@gmail.com>
2025-01-02 06:45:55 +0330

    First Commit

---
+++ JS/app.js
@@ -0,0 +1,97 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 500;
+
+// تصویر هواپیما
+const planeImg = new Image();
+planeImg.src = "./img/plane.webp";
+
+// موقعیت و اندازه‌ی هواپیما
+let planeX = 50;
+let planeY = canvas.height - 60 ;
+const planeWidth = 60;
+const planeHeight = 60;
+
+
+planeImg.onload = () => {
+    animate();
+};
+
+
+let bulletX = planeX+30  ;
+let bulletY = planeY-30;
+var bullets =[];
+
+function animate(){
+    requestAnimationFrame(animate);
+    //جلوگیری از تکرار اشیا بازی در هنگام حرکت
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.drawImage(planeImg, planeX, planeY, planeWidth, planeHeight);
+
+    //شهاب سنگ
+    ctx.fillStyle = "orange";
+    ctx.fillRect(200, 100, 40, 40);
+
+    //  تیر و حرکت آن
+    ctx.fillStyle = "blue";
+    for(let i = bullets.length-1; i>0 ; i--){
+        let b = bullets[i];
+        ctx.fillRect(b.x, b.y, b.width, b.height);
+    }
+
+
+}
+
+
+
+
+
+
+
+document.addEventListener("keydown", (event) => {
+    const step = 10; // مقدار جابه‌جایی در هر بار فشردن کلید
+    if(event.key=="ArrowLeft"){
+        planeX = planeX - step;
+    }else if(event.key=="ArrowRight"){
+        planeX = planeX + step;
+    }
+
+
+
+    
+    // جلوگیری از خروج از مرزهای چپ و راست
+    if (planeX < 0) {
+        planeX = 0;
+    }
+    
+    
+    console.log( canvas.width);
+
+    
+    if (planeX + planeWidth > canvas.width) {
+        planeX = canvas.width - planeWidth;
+    }
+    console.log( planeX + planeWidth);
+
+
+    if(event.key=="space"){
+        BulletThrow();
+    }
+
+
+
+
+
+
+
+
+
+
+
+    
+    // پس از تغییر مختصات، دوباره همه‌چیز را رسم می‌کنیم
+    drawAll();
+});
