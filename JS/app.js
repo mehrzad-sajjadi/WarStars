3199138b6444546157a101afd1cd4506f3c8600a
amirhosein sajjadi <canadavancouver2003@gmail.com>
2025-01-02 08:15:30 +0330

    add LimitBullet

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,12 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+let start = document.getElementById("start");
+
+start.addEventListener("click", () => {
+  animate();
+});
+
 canvas.width = 800;
 canvas.height = 500;
 
@@ -10,21 +16,17 @@ planeImg.src = "./img/plane.webp";
 
 // موقعیت و اندازه‌ی هواپیما
 let planeX = 50;
-let planeY = canvas.height - 60 ;
+let planeY = canvas.height - 60;
 const planeWidth = 60;
 const planeHeight = 60;
 
+// planeImg.onload = () => {
+//     animate();
+// };
 
-planeImg.onload = () => {
-    animate();
-};
+let bullets = [];
 
-
-let bulletX = planeX+30  ;
-let bulletY = planeY-30;
-var bullets =[];
-
-function animate(){
+function animate() {
     requestAnimationFrame(animate);
     //جلوگیری از تکرار اشیا بازی در هنگام حرکت
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -40,65 +42,56 @@ function animate(){
     for (let i = bullets.length - 1; i >= 0; i--) {
         let b = bullets[i];
         ctx.fillRect(b.x, b.y, b.width, b.height);
-        
-        
+
         b.y = b.y - 5;
 
         if (b.y + b.height < 0) {
-            bullets.splice(i, 1);
+        bullets.splice(i, 1);
         }
     }
 }
 
+let bulletNum = document.getElementById("bullet");
 
-function BulletThrow(){
-    let bulletX = planeX+30  ;
-    let bulletY = planeY-30;
+function BulletThrow() {
+    let limitBullet = parseInt(bulletNum.textContent);
 
-    bullets.push({
-        x : bulletX ,
-        y : bulletY ,
-        width : 2,
-        height : 25 
-    });
+    if(limitBullet>0){
+        let bulletX = planeX + 30;
+        let bulletY = planeY - 30;
+    
+        bullets.push({
+            x: bulletX,
+            y: bulletY,
+            width: 2,
+            height: 25,
+        });
+    
+        limitBullet=limitBullet-1; 
+        bulletNum.textContent = limitBullet;     
+    }
 }
-
-
-
-
-
 document.addEventListener("keydown", (event) => {
     const step = 10; // مقدار جابه‌جایی در هر بار فشردن کلید
-    if(event.key=="ArrowLeft"){
+    if (event.key == "ArrowLeft") {
         planeX = planeX - step;
-    }else if(event.key=="ArrowRight"){
+    } else if (event.key == "ArrowRight") {
         planeX = planeX + step;
     }
 
-
-
-    
     // جلوگیری از خروج از مرزهای چپ و راست
     if (planeX < 0) {
         planeX = 0;
     }
-    
-    
-    console.log( canvas.width);
 
-    
+    console.log(canvas.width);
+
     if (planeX + planeWidth > canvas.width) {
         planeX = canvas.width - planeWidth;
     }
-    console.log( planeX + planeWidth);
+    console.log(planeX + planeWidth);
 
-
-    if(event.key == "ArrowUp"){
+    if (event.key == "ArrowUp") {
         BulletThrow();
     }
-    
 });
-
-
-
-
